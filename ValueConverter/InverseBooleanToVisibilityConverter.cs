52b91cadd944ab040b12c6ad851a6551c4151579
Alexey <alexey.tuhtiaev@gmail.com>
2018-04-26 12:43:02 +0300

    IValueConverter

---
+++ ValueConverter/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ValueConverter
+{
+  public  class InverseBooleanToVisibilityConverter:IValueConverter
+    {
+        public object Convert(
+        object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool booleanValue = (bool)value;
+            return booleanValue ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(
+            object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility visibility = (Visibility)value;
+            return visibility != Visibility.Visible;
+        }
+    }
+}
